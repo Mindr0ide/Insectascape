a820ac6358722f21c09a92cddc9419f1e4ebbc97
MelDidntListen <melodyisnotlistening@gmail.com>
2025-04-09 15:48:13 -0700

    Added Parallax + Scene Manager
    3 Scripts for Parallax #4
    - ParallaxLayer
    - ParallaxCamera
    - ParallaxBackground
    
    2 Scripts for Scene Manager #17
    - SceneController
    - LevelSwitch

---
+++ Assets/Scenes/Scripts/SceneManager/SceneController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+//    public static SceneController instance;
+
+//    public void NextLevel()
+//    {
+//         SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+
+//    }
+
+//    public void LoadScene(string sceneName)
+//    {
+//     SceneManager.LoadSceneAsync(sceneName);
+//    }
+}
