2020eade2d5b42ad5d6500c72f712f17bd6e91ea
MelDidntListen <melodyisnotlistening@gmail.com>
2025-04-30 15:53:43 -0700

    Added scenes and menu scripts
    Progress was lost, remaking menu assets.

---
+++ Assets/Scenes/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        Debug.Log("Starting game...");
+        SceneManager.LoadScene("TrainingGrounds");
+    }
+
+    public void OpenSettings()
+    {
+        Debug.Log("Opening settings...");
+        //TODO settings
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log ("Quitting game...");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scenes/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject settingsMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else{
+                Pause();
+            }
+        }
+    } 
+
+    public void Resume ()
+    {
+        settingsMenuUI.SetActive(false);
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause ()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading menu...");
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void OpenSettings()
+    {
+        Debug.Log("Opening settings...");
+        settingsMenuUI.SetActive(true);
+        pauseMenuUI.SetActive(false);
+    }
+
+    public void CloseSettings()
+    {
+        Debug.Log("Closing settings...");
+        settingsMenuUI.SetActive(false);
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log ("Quitting game...");
+        Application.Quit();
+    }
+}
