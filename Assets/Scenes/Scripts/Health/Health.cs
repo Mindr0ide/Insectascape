fa03d7847701ed434cc3676e2544faaace4da026
Mindr0ide <142521261+Mindr0ide@users.noreply.github.com>
2025-05-13 14:51:15 -0700

    re added correct animations for the playe, now you have to hit a jumpad to activate it and you can now press e to attack

--- Assets/Scenes/Scripts/Health/Health.cs
+++ Assets/Scenes/Scripts/Health/Health.cs
@@ -17,14 +17,14 @@ public class Health : MonoBehaviour
 
     public void TakeDamage(float damage)
     {
-        currentHealth = Mathf.Clamp(currentHealth - damage, 0 , maxHealth);
+        currentHealth -= damage;
         if (currentHealth > 0)
         {
             //player hurt
             anim.SetTrigger("hurt");
             StartCoroutine(HurtDelay());
         }
-        else
+        else if (currentHealth == 0)
         {
             //dead
             anim.SetTrigger("die");
@@ -32,6 +32,7 @@ public class Health : MonoBehaviour
             StartCoroutine(RespawnAfterDelay()); 
             //FindObjectOfType<PlayerRespawn>().Respawn();
         }
+        Mathf.Clamp(currentHealth, 0 , maxHealth);
     }
 
     private void Update()
